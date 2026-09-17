ec335de9638facb2b94b326288b4ec29ae90d784
Maroon Shalbak <maroon.shalbak@consensys.net>
2026-09-17 17:03:31 +0300

    fix negative number input for bank balance and frame

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'budget-v4';
+const CACHE = 'budget-v5';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.svg', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
