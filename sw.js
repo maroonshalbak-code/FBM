f8dff6d69e07befe0387450359f326af574002ea
Maroon Shalbak <maroon.shalbak@consensys.net>
2026-09-17 17:29:16 +0300

    enter monthly amount for credit installments, app calculates total

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'budget-v6';
+const CACHE = 'budget-v7';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.svg', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
